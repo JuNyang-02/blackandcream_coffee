9dc7896940a67da0a2e3dbafea90483e83bda8bc
JuNyang-02 <answnsgud19@naver.com>
2025-06-02 10:19:15 +0900

    reset_config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  exportTailingSplash: true,
-  assetPrefix: process.env.NODE_ENV === "production" ? "https://JuNyang-02.github.io/JuNyang-02.github.io" : ""
-};
+const nextConfig = { };
 
 export default nextConfig;
